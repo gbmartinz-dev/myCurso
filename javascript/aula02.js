dc0b69d75d67925cb3656cfa90825af574b1e196
Gabriel Martins <gabrielpereiramartins49@gmail.com>
2024-07-30 19:47:39 -0300

    Aprendendo lógica de programação com Javascript

---
+++ javascript/aula02.js
@@ -0,0 +1,9 @@
+// Operadores matemáticos
+let numero = 10;
+let numerinho = 3;
+
+console.log(numero + numerinho); // adição
+console.log(numero - numerinho); // subtração
+console.log(numero * numerinho); // multiplicação
+console.log(numero / numerinho); // divisão
+console.log(numero % numerinho); // resto da divisão
---
+++ javascript/aula04.js
@@ -0,0 +1,4 @@
+// Operadores lógicos
+console.log(!(10 > 10)); // ! NEGAR.
+console.log((10 == 10) || (10 > 20)); // || OU.
+console.log((10 == 10) && (10 > 5)); // && E.
