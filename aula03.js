d643dbfc166bdf921745dec703164d7b53e4c5d9
Gabriel Martins <gabrielpereiramartins49@gmail.com>
2024-08-01 00:30:00 -0300

    Delete aula03.js

--- aula03.js
+++
@@ -1,9 +0,0 @@
-// Operadores de comparação
-console.log(10 == 10); // == ele retornará true ou false.
-console.log(10 != "10"); // != Diferente de.
-console.log(10 === "10"); // Estritamente igual.
-console.log(10 !== "10"); // Estritamente desigual.
-console.log(10 > 5); // Maior que.
-console.log(10 >= 10); // Maior ou igual.
-console.log(10 < 11); // Menor que.
-console.log(10 <= 10); // Menor ou igual.
